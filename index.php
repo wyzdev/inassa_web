d708943d402f27ca768ded02cc8dcd0d4aa97284
hollyn <hollyn.derisse@esih.edu>
2017-02-15 09:19:51 -0500

    login of the web app

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.css">
+		<link rel="stylesheet" type="text/css" href="css/login_form.css">
+		<title>INASSA</title>
+	</head>
+
+
+	<body>
+	<div class="container-fluid vertical-center">
+		<div class="container">
+		    <div class="row">
+		        <div class="col-sm-6 col-md-4 col-md-offset-4">
+		            <div class="account-wall">
+		                <img class="profile-img" src="http://www.ceshaiti.org/account/acc_files/10000/8038/inassa-logo(1).jpg"
+		                    alt="">
+		                <form class="form-signin">
+		                <input type="text" class="form-control" placeholder="Email" required autofocus>
+		                <input type="password" class="form-control" placeholder="Password" required>
+		                <button class="btn btn-lg btn-primary btn-block" type="submit">
+		                    Sign in</button>
+		                </form>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+	</div>
+	    
+	</body>
+</html>
+
+<style type="text/css">
+	.vertical-center {
+  min-height: 100%; 
+  min-height: 100vh; 
+
+  display: flex;
+  align-items: center;
+}
+</style>
