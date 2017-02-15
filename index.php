631f70e6de8d5958ffed6251a5c48681d4e1b11e
hollyn <hollyn.derisse@esih.edu>
2017-02-15 16:57:21 -0500

    language change and logo added

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
 		    <div class="row">
 		        <div class="col-sm-6 col-md-4 col-md-offset-4">
 		            <div class="account-wall">
-		                <img class="profile-img" src="http://www.ceshaiti.org/account/acc_files/10000/8038/inassa-logo(1).jpg"
+		                <img class="profile-img" src="img/logo_inassa.png"
 		                    alt="">
 		                <form class="form-signin">
-		                <input type="text" class="form-control" placeholder="Email" required autofocus>
-		                <input type="password" class="form-control" placeholder="Password" required>
+		                <input type="text" class="form-control" placeholder="Nom d'utilisateur" required autofocus>
+		                <input type="password" class="form-control" placeholder="Mot de passe" required>
 		                <button class="btn btn-lg btn-primary btn-block" type="submit">
-		                    Sign in</button>
+		                    Connexion</button>
 		                </form>
 		            </div>
 		        </div>
